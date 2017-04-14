6d38c2a5bd47889acbb0f2c997c2fe9309a1ac8b
Djimgou Nkenne <danmfacto@gmail.com>
2017-04-14 15:33:43 +0200

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -3,11 +3,6 @@
 $db = "etudiantsdb";
 $pswd = "";
 $user = "root";
-if ($_SERVER['HTTP_HOST'] == "projet-web-m2itic.comeze.com") {
-    $user = "id870773_root";
-    $db = "id870773_etudiantsdb";
-    $pswd = "mysql";
-}
 
 @mysql_connect("localhost",$user, $pswd) or die("Echec de connexion au serveur.");
 @mysql_select_db($db) or die("Echec de sélection de la base.");
